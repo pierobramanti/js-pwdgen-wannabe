048ad5b748d630fb4107ac131fc4cdc7ffeb1309
pierobramanti <piero.bramanti94@gmail.com>
2024-06-24 16:36:05 +0200

    [UPD]inserted initialization

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let name = prompt("Inserisci il tuo nome")
+let fname = prompt("Inserisci il tuo nome")
 let surname = prompt("Innserisci il tuo congnome")
 let color = prompt("Inserisci il tuo colore preferito")
 
+let answers = `${fname}${ surname}${color}23`
