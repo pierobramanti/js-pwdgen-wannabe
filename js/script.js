27ab4009837bbe9d1e7db944accb52b6c77360e0
pierobramanti <piero.bramanti94@gmail.com>
2024-06-24 16:55:17 +0200

    [UPD] assigned the last variable to #property

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@ let fname = prompt("Inserisci il tuo nome")
 let surname = prompt("Innserisci il tuo congnome")
 let color = prompt("Inserisci il tuo colore preferito")
 
-let answers = `${fname}${ surname}${color}23`
+let answers = `${fname}${surname}${color}23`
+
+document.getElementById("property").innerHTML = answers;
